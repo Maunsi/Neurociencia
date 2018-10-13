63d5867bfdd418507943d12f352c9285d654dace
SEBPort0 <sebassi@live.com>
2018-10-13 00:24:00 -0300

    añadido: mascaras, primers y mascara para los flankers. Pequeño problema: flanker derecho sube un poco de lugar despues de mostrar la mascara, el izquierdo no.

--- addExp.py
+++ addExp.py
@@ -28,6 +28,16 @@ def bienvenida(win):
 #crear una ventana
 win=visual.Window(fullscr=True)
 
+# mascara
+mascara = visual.TextStim(win=win, name='mascara', text='MWMWMWMWMWM', units='norm', pos=(0, 0))
+
+# primers
+primerSumar = visual.TextStim(win=win, name='sumar', text='SUMAR', units='norm', pos=(0, 0))
+primerRepresentar = visual.TextStim(win=win, name='REPRESENTAR', units='norm', pos=(0, 0))
+
+# mascaras para los flankers
+mascaraFlankerLeft = visual.TextStim(win=win, name='mascaraFlankerLeft', text='##', units='norm', pos=(-0.5, 0))
+mascaraFlankerRight = visual.TextStim(win=win, name='mascaraFlankerRight', text='##', units='norm', pos=(0.5, 0))
 
 listPairs = []
 
@@ -36,8 +46,7 @@ pairFile = open("pairInputs.txt", "r")
 for line in pairFile:
     left, right = line.split(",")
     textLeft = visual.TextStim(win=win, name='textLeft', text=left, units='norm', pos=(-0.5, 0))
-    
-    textRight = visual.TextStim(win=win, name='textRight', text=right, units= 'norm', pos=(0.5, 0))
+    textRight = visual.TextStim(win=win, name='textRight', text=right, units='norm', pos=(0.5, 0))
     
     listPairs.append(textLeft)
     listPairs.append(textRight)
@@ -57,24 +66,53 @@ clock = core.Clock()
 bienvenida(win)
 
 event.clearEvents()
-while len(listPairs) != 0:  #corro mientras queden estimulos    
-    1#Mostrar pares
+while len(listPairs) != 0:  #corro mientras queden estimulos
+    
+    # mostrar mascara
+    mascara.draw()
+    win.flip()
+    core.wait(1)
+    
+    # mostrar primer. Por ahora solo muestra 'sumar'.
+    primerSumar.draw()
+    win.flip()
+    core.wait(1)
+    
+    # mostrar mascara
+    mascara.draw()
+    win.flip()
+    core.wait(1)
+    
+    # mostrar mascara para los flankers
+    mascaraFlankerLeft.draw()
+    mascaraFlankerRight.draw()
+    win.flip()
+    core.wait(1)
+    
+    #Mostrar pares
     listPairs.pop(0).draw()
     listPairs.pop(0).draw()
     win.flip()
     core.wait(0.5)
     
+    # mostrar mascara para los flankers
+    mascaraFlankerLeft.draw()
+    mascaraFlankerRight.draw()
+    core.wait(1)
+    win.flip()
+    
     #Mostrar resultado
     listRes.pop(0).draw()
     win.flip() 
     core.wait(0.5)
+    
 
 list = event.getKeys(keyList=["space"], timeStamped=True)
 
 #file = open("ej1.txt","a")
 #file.write(list)
 #file.close()
-with open('ej2.txt', 'a') as f:
+with open('addExp.txt', 'a') as f:
     for item in list:
         f.write("%s\n" % str(item))
 
