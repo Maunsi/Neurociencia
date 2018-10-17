f8e5a5c31b97333ea4d7b87d9117d9e644aee556
Iair <iair.embon@gmail.com>
2018-10-17 15:58:27 -0300

    Escribe los resultados del trial en el mismo orden en que el generador los genera. Sumar num correcto Sumar num incorrecto Sumar letra Sumar letra Representar num correcto Representar num incorrecto Representar letra Representar letra

--- addExp.py
+++ addExp.py
@@ -22,20 +22,23 @@ def draw(win, stimuli, time):
 	win.flip()
 	core.wait(time)
 
-def crearTextos():
-    textList = []
-    inputFile = open("pairAndResInputs.txt", "r")
-    for line in inputFile:
-        # rstrip() para evitar que algun \n moleste
-        pairString, resString, primerString = line.rstrip().split(" ")
-        leftString, rightString = pairString.split(",")
-        textLeft = visual.TextStim(win=win, name='textLeft', text=leftString, units='norm', pos=(-0.5, 0))
-        textRight = visual.TextStim(win=win, name='textRight', text=rightString, units= 'norm', pos=(0.5, 0))
-        textRes =  visual.TextStim(win=win, name='textRes', text=resString, units= 'norm', pos=(0, 0))
-        textPrimer = visual.TextStim(win=win, name='textPrimer', text=primerString, units='norm', pos=(0, 0))
-        textList.append(((textLeft, textRight), textRes, textPrimer))
-    inputFile.close()
-    return textList
+def read_input_file():
+	input_list = []
+	input_file = open("pairAndResInputs.txt", "r")
+	for line in input_file:
+		# rstrip() para evitar que algun \n moleste
+		trial_id, prime, pairString, res = line.rstrip().split(" ")
+		left, right = pairString.split(",")
+		input_list.append((int(trial_id), prime, left, right, res))
+	input_file.close()
+	return input_list
+	
+def generate_texts(prime, left, right, res):
+	textPrime = visual.TextStim(win=win, name='textPrime', text=prime, units='norm', pos=(0, 0))
+	textLeft = visual.TextStim(win=win, name='textLeft', text=left, units='norm', pos=(-0.5, 0))
+	textRight = visual.TextStim(win=win, name='textRight', text=right, units= 'norm', pos=(0.5, 0))
+	textRes =  visual.TextStim(win=win, name='textRes', text=res, units= 'norm', pos=(0, 0))
+	return textPrime, textLeft, textRight, textRes
 
 ################################################################################################################
 #crear una ventana
@@ -47,15 +50,9 @@ centro = visual.TextStim(win=win, name='centro', text='| |', units='norm', pos=(
 # mascara
 mascara = visual.TextStim(win=win, name='mascara', text='MWMWMWMWMWM', units='norm', pos=(0, 0))
 
-# primers
-#primerSumar = visual.TextStim(win=win, name='sumar', text='SUMAR', units='norm', pos=(0, 0))
-#primerRepresentar = visual.TextStim(win=win, name='REPRESENTAR', units='norm', pos=(0, 0))
-
 # mascaras para los flankers
 mascaraFlankerLeft = visual.TextStim(win=win, name='mascaraFlankerLeft', text='##', units='norm', pos=(-0.5, 0))
 mascaraFlankerRight = visual.TextStim(win=win, name='mascaraFlankerRight', text='##', units='norm', pos=(0.5, 0))
-
-textList= crearTextos()
 
 #crear el objeto clock que sirve para controlar el tiempo (clock cuenta en segundos)
 clock = core.Clock()
@@ -65,41 +62,52 @@ bienvenida(win)
 #idea para obtener teclas: hago clear antes de res, y hago getKeys despues de res
 event.clearEvents()
 pressedKeys = []
-while len(textList) != 0:  #corro mientras queden estimulos
-    
+
+input_list = read_input_file()
+random.shuffle(input_list)
+n = len(input_list)
+trial_by_id = {}
+trial_responses_by_id = {}
+while len(input_list) != 0:  #corro mientras queden estimulos
+	trial_id, prime, left, right, res = input_list.pop(0)
     # prepara target, flankers y primers
-    ((textLeft, textRight), textRes, textPrimer) = textList.pop(0)
+	textPrime, textLeft, textRight, textRes = generate_texts(prime, left, right, res)
     
     # mostrar el centro
-    draw(win, {centro}, 1)
+	draw(win, {centro}, 1)
     
     # mostrar mascara    
-    draw(win, {mascara}, 1)
+	draw(win, {mascara}, 1)
     
     # mostrar primer de acuerdo a lo especificado en pairAndResInputs.txt
-    draw(win, {textPrimer}, 1)
+	draw(win, {textPrime}, 1)
     
     # mostrar mascara
-    draw(win, {mascara}, 1)
+	draw(win, {mascara}, 1)
     
     # mostrar mascara para los flankers junto con el centro
-    draw(win, {centro, mascaraFlankerLeft, mascaraFlankerRight}, 1)
+	draw(win, {centro, mascaraFlankerLeft, mascaraFlankerRight}, 1)
     
     #Mostrar pares y el centro
-    draw(win, {centro, textLeft, textRight}, 1)
+	draw(win, {centro, textLeft, textRight}, 1)
     
     # mostrar mascara para los flankers y el centro
-    draw(win, {centro, mascaraFlankerLeft, mascaraFlankerRight}, 1)
+	draw(win, {centro, mascaraFlankerLeft, mascaraFlankerRight}, 1)
     
     #Mostrar resultado y mascaras para los flankers
-    draw(win, {textRes, mascaraFlankerLeft, mascaraFlankerRight}, 1)
+	draw(win, {textRes, mascaraFlankerLeft, mascaraFlankerRight}, 1)
     
     # prueba: igual que getKeys pero espera el tiempo indicado por maxWait, tal vez sirve para 
     # cortar la prueba en caso de demora o respuesta correcta.
-    pressedKeys.append(event.waitKeys(maxWait=1, keyList=["left", "right"], timeStamped=True))
-    #pressedKeys.append(event.getKeys(keyList=["left", "right"], timeStamped=True))
+	
+	trial_by_id[trial_id] = (prime, left, right, res)
+	keys = []
+	keys.append(event.waitKeys(maxWait=2, keyList=["left", "right"], timeStamped=True))
+	trial_responses_by_id[trial_id] = keys
 
-with open("pressedKeys", "w") as f:
-    for item in pressedKeys:
-        f.write("%s\n" % str(item))
+with open("results.txt", "w") as f:
+	for i in range(n):
+		(prime, left, right, res) = trial_by_id[i]
+		response = trial_responses_by_id[i]
+		f.write("%s\n" % str(i) + " " + prime + " " + left + " " + right + " " + res + " " + str(response))
 f.close()
--- generadorDePares.py
+++ generadorDePares.py
@@ -1,33 +1,38 @@
 import random, sys, string
 
+def generarMitadPrime(prime, start, end, input_list):
+	mitad = (start + end)
+	for i in range(start, end):
+		print(i)
+		left = random.randint(1,5)
+		right = random.randint(1,5)
+		res = 0
+		if(i < mitad//4):
+			res = left + right
+		elif( i >= mitad//4 and i < mitad//2):
+			res = random.randint(1,5)
+			while ((res == (left + right)) or (res == left) or (res == right)):
+				res = random.randint(1,5)
+		else:
+			res = random.choice(['A', 'B', 'C', 'D'])
+		input_list.append((i, prime, left, right, res))
 
 #genero los pares, los guardo en un diccionario y luego randomizo las keys para poder escribirlo ya "desordenado"
 if(len(sys.argv) != 2):
 	print("Este script toma como parametro la cantidad de estimulos a generar")
 else:
 	n = int(sys.argv[1])
-	inputList = []
-	for i in range(n):
-		print(i)
-		left = random.randint(1,5)
-		right = random.randint(1,5)
-		res = 0
-		if(i < n//4):
-			res = left + right
-		elif( i >= n//4 and i < n//2):
-			res = random.randint(1,5)
-			while ((res == (left + right)) or (res == left) or (res == right)):
-				res = random.randint(1,5)
-		else:
-			res = random.choice(string.ascii_uppercase)
-		inputList.append(((left, right), res))
+	
+	
+	input_list = []
+	#Esta mitad corresponde a sumar
+	
+	generarMitadPrime('sumar', 0, n//2, input_list)
+	generarMitadPrime('representar', n//2, n, input_list)
 
-	print(inputList)
-	random.shuffle(inputList)
-	print(inputList)
 	f = open("pairAndResInputs.txt", "w")
-	for ((left, right), res) in inputList:
-		s = str(left) + "," + str(right) + " " + str(res)
+	for (i, prime, left, right, res) in input_list:
+		s = str(i) + " " + prime + " " + str(left) + "," + str(right) + " " + str(res)
 		f.write("%s\n" % s)
 	f.close()
 
