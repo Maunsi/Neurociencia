444c739783351db4c518ca42c0986b06905813eb
Maunsi <maukeegan1@gmail.com>
2018-11-08 23:46:49 -0300

    Agregue el control de pares

--- analizador_csv.py
+++ analizador_csv.py
@@ -70,7 +70,8 @@ def leer_resultados():
 def analizar(df):
 	df_menores_a_cuatro = filtrar_mayores_a_cuatro(df)
 	df_nuevo = filtrar_pruebas_letra(df_menores_a_cuatro)
-	analisis_control_objetivo(df_nuevo)
+	analisis_control_objetivo_operacion(df_nuevo)
+	analisis_control_objetivo_pares(df_nuevo)
 
 def filtrar_mayores_a_cuatro(df):
 	sujetos_antes = len(df["Sujeto"].unique())
@@ -94,7 +95,7 @@ def filtrar_pruebas_letra(df):
 	#	print(df)
 	return df
 
-def analisis_control_objetivo(df):
+def analisis_control_objetivo_operacion(df):
 	# L = SUMAR, A = REPRESENTAR.
 	d_primas = []
 	hits_totales, misses_totales, falsas_alarmas_totales, correct_rejections_totales= 0, 0, 0, 0
@@ -125,6 +126,50 @@ def analisis_control_objetivo(df):
 	# 	d_prima = 1/promedio_hits - 1/promedio_falsas_alarmas
 	# 	d_primas.append(d_prima)
 	print "Hits totales: {}, Misses totales: {}, Falsas alarmas totales: {}, Correct Rejections totales: {}".format(hits_totales, misses_totales, falsas_alarmas_totales, correct_rejections_totales)
+	plt.bar([0,1,2,3], [hits_totales, misses_totales, falsas_alarmas_totales, correct_rejections_totales])  # arguments are passed to np.histogram
+	plt.xticks([0,1,2,3], ["hits", "misses", "false alarms", "correct rejections"])
+	plt.title("Hits Misses Falsas alarmas Rechazos correctos")
+	plt.show()
+	# #Tengo la lista de d's
+	# t = stats.ttest_1samp(d_primas, 0)
+	# print "T-test result: {}".format(t)
+
+def analisis_control_objetivo_pares(df):
+	#with pandas.option_context('display.max_rows', None, 'display.max_columns', None):
+	#	print(df)
+	#PARA LO DE NONES HABRIA QUE SELECCIONAR SOLO LAS FILAS DONDE HAY FLANKER IZQUIERDO. 
+	#PARECE QUE CUANDO ERA IMPAR QUIZAS LA GENTE RESPONDIO MUY TEMPRANO?
+	hits_totales, misses_totales, falsas_alarmas_totales, correct_rejections_totales, nones_totales = 0, 0, 0, 0, 0
+	sujetos = df.Sujeto.unique()
+	print sujetos
+	for sujeto in sujetos:
+		hits, misses, falsas_alarmas, correct_rejections, nones = 0, 0, 0, 0, 0
+		#Para cada row de este sujeto
+		for index, row in df.loc[df["Sujeto"] == sujeto].iterrows():
+			par = row["Flanker_izquierdo"] % 2 == 0
+			respuesta = row["Control_pares"]
+			if respuesta is None:
+				nones +=1
+			elif  par and respuesta == 'par': # Si el flanker izquierdo era par y respondi par es un hit
+				hits += 1
+			elif par and respuesta == 'impar': # Si el flanker izquierdo era par y respondi impar es un miss
+				misses +=1
+			elif not(par) and respuesta == 'par': # Si el flanker izquierdo era impar y respondi par es una falsa alarma
+				falsas_alarmas +=1
+			elif not(par) and respuesta == 'impar': # Si el flanker izquierdo era impar y respondi impar es una correct rejection
+				correct_rejections +=1
+		hits_totales += hits
+		misses_totales += misses
+		falsas_alarmas_totales += falsas_alarmas
+		correct_rejections_totales += correct_rejections
+		nones_totales += nones
+		print "Sujeto: {}, Hits: {}, Misses: {}, Falsas alarmas: {}, Correct Rejections: {}, Nones: {}".format(sujeto, hits, misses, falsas_alarmas, correct_rejections, nones)
+	# 	probabilidad_hit = hits/(hits + misses) #hits dividido todos los trials que tuvieron como prime sumar
+	# 	probabilidad_falsa_alarma =  falsas_alarmas/(falsas_alarmas + correct_rejections) 
+	# 	#falsas alarmas dividido todos los trials que tuvieron como prime representar
+	# 	d_prima = 1/promedio_hits - 1/promedio_falsas_alarmas
+	# 	d_primas.append(d_prima)
+	print "Hits totales: {}, Misses totales: {}, Falsas alarmas totales: {}, Correct Rejections totales: {}, Nones totales: {}".format(hits_totales, misses_totales, falsas_alarmas_totales, correct_rejections_totales, nones_totales)
 	plt.bar([0,1,2,3], [hits_totales, misses_totales, falsas_alarmas_totales, correct_rejections_totales])  # arguments are passed to np.histogram
 	plt.xticks([0,1,2,3], ["hits", "misses", "false alarms", "correct rejections"])
 	plt.title("Hits Misses Falsas alarmas Rechazos correctos")
